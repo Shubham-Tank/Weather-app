2c66a8f0b52beff06308eea4a2a167a5e2f36ff8
Shubham Tank <tankshubham3322@gmail.com>
2022-04-19 01:41:59 +0530

    add datetime

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -8,106 +8,137 @@ import { WiHumidity } from "react-icons/wi";
 import Tooltip from "@mui/material/Tooltip";
 
 const Weather = ({ lat, lon, country }) => {
-  const [weatherData, setWeatherData] = useState(null);
+	const [weatherData, setWeatherData] = useState(null);
 
-  const fetchWeather = async () => {
-    const response = await axios.get(urls.weather(lat, lon));
-    if (JSON.stringify(weatherData) !== JSON.stringify(response.data))
-      setWeatherData(response.data);
-  };
+	const fetchWeather = async () => {
+		const response = await axios.get(urls.weather(lat, lon));
+		if (JSON.stringify(weatherData) !== JSON.stringify(response.data))
+			setWeatherData(response.data);
+	};
 
-  useEffect(() => {
-    fetchWeather();
-  });
+	useEffect(() => {
+		fetchWeather();
+	});
 
-  const date = new Date();
+	const date = new Date();
 
-  return (
-    weatherData && (
-      <Box sx={{ flexGrow: 1 }} className="weather-container">
-        <Grid container spacing={2}>
-          <Grid item xs={6} className="weather-info">
-            <div className="temp">
-              <BsThermometerHalf style={{ verticalAlign: "text-bottom" }} />
-              {Math.round(weatherData.main.temp - 273.15)} <sup>°C</sup>
-            </div>
-            <div className="lite weather-subinfo">
-              <h4>
-                <Tooltip
-                  title={`Humidity: ${weatherData.main.humidity}%`}
-                  placement="right"
-                  arrow
-                >
-                  <span className="lite">
-                    <WiHumidity
-                      style={{
-                        verticalAlign: "sub",
-                        fontSize: "1.5rem",
-                      }}
-                    />
-                    {weatherData.main.humidity}%
-                  </span>
-                </Tooltip>
-              </h4>
+	const days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
 
-              <h4>
-                <Tooltip
-                  title={`Wind: ${Math.floor(
-                    weatherData.wind.speed * 3.6
-                  )} Km/h`}
-                  placement="right"
-                  arrow
-                >
-                  <span className="lite">
-                    <BsWind
-                      style={{
-                        verticalAlign: "text-bottom",
-                        marginRight: ".2rem",
-                      }}
-                    />
-                    {Math.floor(weatherData.wind.speed * 3.6)} Km/h
-                  </span>
-                </Tooltip>
-              </h4>
+	return (
+		weatherData && (
+			<Box sx={{ flexGrow: 1 }} className="weather-container">
+				<Grid container spacing={2}>
+					<Grid item xs={6} className="weather-info">
+						<div className="temp">
+							<BsThermometerHalf style={{ verticalAlign: "text-bottom" }} />
+							{Math.round(weatherData.main.temp - 273.15)} <sup>°C</sup>
+						</div>
+						<div className="lite weather-subinfo">
+							<h4>
+								<Tooltip
+									title={`Humidity: ${weatherData.main.humidity}%`}
+									placement="right"
+									arrow
+								>
+									<span className="lite">
+										<WiHumidity
+											style={{
+												verticalAlign: "sub",
+												fontSize: "1.5rem",
+											}}
+										/>
+										{weatherData.main.humidity}%
+									</span>
+								</Tooltip>
+							</h4>
 
-              <h4>
-                <Tooltip
-                  title={`Atmospheric Pressure: ${weatherData.main.pressure} hPa`}
-                  placement="right"
-                  arrow
-                >
-                  <span className="lite">
-                    <BsSpeedometer
-                      style={{
-                        verticalAlign: "text-bottom",
-                        marginRight: ".2rem",
-                      }}
-                    />
-                    {weatherData.main.pressure} hPa
-                  </span>
-                </Tooltip>
-              </h4>
-            </div>
-          </Grid>
-          <Grid item xs={6} style={{ textAlign: "right" }}>
-            <h2 className="pro">
-              {weatherData.name}, {country}
-            </h2>
-            <h4 className="lite">{date.toDateString()}</h4>
-            <h4 className="lite">{date.toLocaleTimeString()}</h4>
-          </Grid>
-          <Grid item xs={12} style={{ textAlign: "center" }}>
-            <div className="weather">
-              <img
-                src={` http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
-              />
-              <h1 className="pro">{weatherData.weather[0].description}</h1>
-            </div>
-          </Grid>
-        </Grid>
-      </Box>
-    )
-  );
+							<h4>
+								<Tooltip
+									title={`Wind: ${Math.floor(
+										weatherData.wind.speed * 3.6
+									)} Km/h`}
+									placement="right"
+									arrow
+								>
+									<span className="lite">
+										<BsWind
+											style={{
+												verticalAlign: "text-bottom",
+												marginRight: ".2rem",
+											}}
+										/>
+										{Math.floor(weatherData.wind.speed * 3.6)} Km/h
+									</span>
+								</Tooltip>
+							</h4>
+
+							<h4>
+								<Tooltip
+									title={`Atmospheric Pressure: ${weatherData.main.pressure} hPa`}
+									placement="right"
+									arrow
+								>
+									<span className="lite">
+										<BsSpeedometer
+											style={{
+												verticalAlign: "text-bottom",
+												marginRight: ".2rem",
+											}}
+										/>
+										{weatherData.main.pressure} hPa
+									</span>
+								</Tooltip>
+							</h4>
+						</div>
+					</Grid>
+					<Grid item xs={6} style={{ textAlign: "right" }}>
+						<h2 className="pro">
+							{weatherData.name}, {country}
+						</h2>
+						<h4 className="lite">{lat.toFixed(4)}° N</h4>
+						<h4 className="lite">{lon.toFixed(4)}° E</h4>
+					</Grid>
+					<Grid
+						item
+						xs={12}
+						style={{
+							textAlign: "center",
+							display: "flex",
+							justifyContent: "space-around",
+							alighItems: "center",
+							flexWrap: "wrap",
+						}}
+					>
+						<div className="weather">
+							<img
+								src={` http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+							/>
+							<h1 className="pro">{weatherData.weather[0].description}</h1>
+						</div>
+						<div className="date-time">
+							<h1>
+								<span>{days[date.getDay()]} </span>
+								<span className="time">
+									{`${date.getHours() % 12}:${date
+										.toLocaleTimeString()
+										.slice(3, 5)} ${date.getHours > 12 ? "pm" : "am"}`}
+								</span>
+							</h1>
+							<h2 className="lite">{date.toDateString().slice(4)}</h2>
+						</div>
+					</Grid>
+				</Grid>
+			</Box>
+		)
+	);
 };
 
 export default Weather;
