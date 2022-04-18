51f0c8c4651c536f772c0be521f69d2a3df1d495
Shubham Tank <tankshubham3322@gmail.com>
2022-04-18 18:10:25 +0530

    add basic weather info

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,68 +1,75 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import urls from '../apiUrls'
-import SearchContries from './SearchContries'
-import SearchCities from './SearchCities'
-import Weather from './Weather'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import urls from "../apiUrls";
+import SearchContries from "./SearchContries";
+import SearchCities from "./SearchCities";
+import Weather from "./Weather";
 
 const Search = () => {
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [latLon, setLatLon] = useState({ lat: null, lon: null });
 
-    const [country, setCountry] = useState('')
-    const [city, setCity] = useState('')
-    const [latLon, setLatLon] = useState({ lat: null, lon: null })
+  const [allCountries, setAllCountries] = useState([]);
+  const [cityList, setCityList] = useState([]);
 
-    const [allCountries, setAllCountries] = useState([])
-    const [cityList, setCityList] = useState([])
+  let source = axios.CancelToken.source();
+  let config = { cancelToken: source.token };
 
-    let source = axios.CancelToken.source()
-    let config = { cancelToken: source.token }
+  const fetchCountries = async () => {
+    const response = await axios.get(urls.countries);
+    setAllCountries(response.data.data);
+  };
 
-    const fetchCountries = async () => {
-        const response = await axios.get(urls.countries)
-        setAllCountries(response.data.data)
-    }
+  const fetchCities = async (country) => {
+    const response = await axios.post(
+      urls.cities,
+      {
+        country: country.toLowerCase(),
+      },
+      config
+    );
+    setCityList(response.data.data);
+  };
 
-    const fetchCities = async (country) => {
-        const response = await axios.post(urls.cities, {
-            "country": country.toLowerCase()
-        }, config)
-        setCityList(response.data.data)
-    }
+  const fetchLatLon = async (city) => {
+    const response = await axios.get(urls.latLon(city));
+    const { lat, lon } = response.data[0];
+    setLatLon({ lat, lon });
+  };
 
-    const fetchLatLon = async (city) => {
-        const response = await axios.get(urls.latLon(city))
-        const { lat, lon } = response.data[0]
-        setLatLon({ lat, lon })
-    }
+  useEffect(() => {
+    fetchCountries();
+  }, []);
 
-    useEffect(() => {
-        fetchCountries()
-    }, [])
+  useEffect(() => {
+    if (country) fetchCities(country);
 
-    useEffect(() => {
-        if (country)
-            fetchCities(country)
+    return () => {
+      source.cancel();
+    };
+  }, [country]);
 
-        return () => {
-            source.cancel()
-        }
-    }, [country])
+  useEffect(() => {
+    if (city) fetchLatLon(city);
+  }, [city]);
 
-    useEffect(() => {
-        if (city)
-            fetchLatLon(city)
-    }, [city])
+  return (
+    <section className="search">
+      <header>
+        <h1 className="heading">Weather App</h1>
+      </header>
+      <SearchContries
+        countries={allCountries}
+        onCountryChange={(value) => setCountry(value)}
+      />
+      <SearchCities
+        cities={cityList}
+        onCityChange={(value) => setCity(value)}
+      />
+      {latLon.lat && <Weather {...latLon} country={country} />}
+    </section>
+  );
+};
 
-    return (
-        <section className='search'>
-            <header>
-                <h1 className='heading'>Weather App</h1>
-            </header>
-            <SearchContries countries={allCountries} onCountryChange={(value) => setCountry(value)} />
-            <SearchCities cities={cityList} onCityChange={(value) => setCity(value)} />
-            {latLon.lat && <Weather {...latLon} />}
-        </section>
-    )
-}
-
-export default Search
+export default Search;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,21 +1,113 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import urls from '../apiUrls'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import urls from "../apiUrls";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { BsThermometerHalf, BsWind, BsSpeedometer } from "react-icons/bs";
+import { WiHumidity } from "react-icons/wi";
+import Tooltip from "@mui/material/Tooltip";
 
-const Weather = ({ lat, lon }) => {
+const Weather = ({ lat, lon, country }) => {
+  const [weatherData, setWeatherData] = useState(null);
 
-    const fetchWeather = async () => {
-        const response = await axios.get(urls.weather(lat, lon))
-        console.log(response)
-    }
+  const fetchWeather = async () => {
+    const response = await axios.get(urls.weather(lat, lon));
+    if (JSON.stringify(weatherData) !== JSON.stringify(response.data))
+      setWeatherData(response.data);
+  };
 
-    useEffect(() => {
-        fetchWeather()
-    })
+  useEffect(() => {
+    fetchWeather();
+  });
 
-    return (
-        <div>Weather</div>
+  const date = new Date();
+
+  return (
+    weatherData && (
+      <Box sx={{ flexGrow: 1 }} className="weather-container">
+        <Grid container spacing={2}>
+          <Grid item xs={6} className="weather-info">
+            <div className="temp">
+              <BsThermometerHalf style={{ verticalAlign: "text-bottom" }} />
+              {Math.round(weatherData.main.temp - 273.15)} <sup>°C</sup>
+            </div>
+            <div className="lite weather-subinfo">
+              <h4>
+                <Tooltip
+                  title={`Humidity: ${weatherData.main.humidity}%`}
+                  placement="right"
+                  arrow
+                >
+                  <span className="lite">
+                    <WiHumidity
+                      style={{
+                        verticalAlign: "sub",
+                        fontSize: "1.5rem",
+                      }}
+                    />
+                    {weatherData.main.humidity}%
+                  </span>
+                </Tooltip>
+              </h4>
+
+              <h4>
+                <Tooltip
+                  title={`Wind: ${Math.floor(
+                    weatherData.wind.speed * 3.6
+                  )} Km/h`}
+                  placement="right"
+                  arrow
+                >
+                  <span className="lite">
+                    <BsWind
+                      style={{
+                        verticalAlign: "text-bottom",
+                        marginRight: ".2rem",
+                      }}
+                    />
+                    {Math.floor(weatherData.wind.speed * 3.6)} Km/h
+                  </span>
+                </Tooltip>
+              </h4>
+
+              <h4>
+                <Tooltip
+                  title={`Atmospheric Pressure: ${weatherData.main.pressure} hPa`}
+                  placement="right"
+                  arrow
+                >
+                  <span className="lite">
+                    <BsSpeedometer
+                      style={{
+                        verticalAlign: "text-bottom",
+                        marginRight: ".2rem",
+                      }}
+                    />
+                    {weatherData.main.pressure} hPa
+                  </span>
+                </Tooltip>
+              </h4>
+            </div>
+          </Grid>
+          <Grid item xs={6} style={{ textAlign: "right" }}>
+            <h2 className="pro">
+              {weatherData.name}, {country}
+            </h2>
+            <h4 className="lite">{date.toDateString()}</h4>
+            <h4 className="lite">{date.toLocaleTimeString()}</h4>
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+            <div className="weather">
+              <img
+                src={` http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+              />
+              <h1 className="pro">{weatherData.weather[0].description}</h1>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
     )
-}
+  );
+};
 
-export default Weather
+export default Weather;
